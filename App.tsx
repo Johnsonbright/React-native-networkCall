8c08d0bd54771be462969ba73031f68498761663
Mac <mac@Macs-MacBook-Pro.local>
2024-09-17 15:21:37 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import Root from './src/navigation/root';
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-function App(): JSX.Element {
-  return <Root />;
+const App = () => {
+const [isLoading, setIsLoading] = useState(false)
+const [data, setData] = useState(null)
+
+useEffect(() => {
+  FetchData()
+}, [])
+
+const FetchData = async() => {
+  setIsLoading(true)
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+
+  const data = await response.json();
+ setData(data)
+   setIsLoading(false)
+ 
 }
 
-export default App;
+  return (
+    <View>
+
+      {
+        isLoading? <ActivityIndicator size={100}/> : null
+      }
+      <Button title="Make API call" onPress={FetchData} />
+      {data ? (
+        <View>
+          <Text>Title- {data.title}</Text>
+          <Text>Body- {data.body}</Text>
+        </View>
+      ): null}
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
