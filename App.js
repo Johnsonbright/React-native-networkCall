ef9c842fab4dcf748b367931f8f869b37b464a9a
Ifechukwu Johnson Uzoma <johnsonbright101@gmail.com>
2024-12-11 21:19:30 +0100

    size of indicator

--- App.js
+++ App.js
@@ -1,65 +1,43 @@
-import { FlatList, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
 const App = () => {
- const [product, setProducts] = useState([]);
-console.log('Products ', product)
-
- useEffect(() => {
-  getAPIProduct()
- }, [])
-
-  const getAPIProduct = async() => {
-    
-    const response = await fetch ("https://fakestoreapi.com/products")
-    const data = await response.json()
-    console.log(data.image)
-    setProducts(data)
- 
-  }
-
-  return (
-    <SafeAreaView>
-    <FlatList data={product} renderItem={ProductCard} keyExtractor={item => item.id}/>
-    </SafeAreaView>
-  )
+  const [isLoading, setIsLoading] = useState(false)
+const postData = {
+  title: "This is my first time",
+  user: "Zero deegree coder"
 }
 
-const ProductCard = ({item}) => {
-  return(
-    <View style={styles.productCard}>
-    
-      <Text style={styles.title}>{item.title}</Text>
-      <Image source={{uri: item.image}} styles={styles.image}/>
-    
-      <Text style={styles.price}> Price: ₦{item.price}</Text>
-      <Text style={styles.price}> Category: {item.category}</Text>
+  const handleCreatePost = async () => {
+    try{
+      setIsLoading(true)
+      // const api_url = " http://192.168.202.96:3000/Post"
+      const api_url = "http://localhost:3000/Post"
+
+      const response = await fetch(api_url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+           body: JSON.stringify(postData),
+      })
+      const data = await response.json();
+      console.log("DATA", data)
+    } catch (error) {
+      setIsLoading(false)
+      console.error(error)
+    }
+
+  }
+  return (
+    <View>
+      <Text>Learn Post Request in React Native</Text>
+     <Button  title="Create Post" onPress={handleCreatePost}/>
+    {isLoading ?  <ActivityIndicator size="medium"/> : null }
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  productCard:{
-    backgroundColor:'#EBDBAD',
-    borderWidth: 1,
-    borderColor: 'gray',
-    elevation: 20,
-    padding:10,
-    margin: 15,
-    borderRadius: 10
-  },
-  image: {
-    height: 200,
-    resizeMode:'cover'
-  },
-  title: {
-    color: 'black',
-    fontSize: 20
-  },
-  price: {
-    color: 'black',
-    fontSize: 18
-  }
-})
+const styles = StyleSheet.create({})
